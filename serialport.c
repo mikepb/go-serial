76faf90a6d7105cd8d2a597e387ea919891231f7
Kalle Møller (KM) <km@conscia.com>
2018-09-13 13:51:32 +0200

    Fixed error when a blocking read had a timeout and no data was ever read

--- serialport.c
+++ serialport.c
@@ -916,9 +916,11 @@ SP_API enum sp_return sp_blocking_read(struct sp_port *port, void *buf,
 	}
 
 	/* Start background operation for subsequent events. */
-	if (WaitCommEvent(port->hdl, &port->events, &port->wait_ovl) == 0) {
-		if (GetLastError() != ERROR_IO_PENDING)
-			RETURN_FAIL("WaitCommEvent() failed");
+	if (bytes_read > 0) {
+		if (WaitCommEvent(port->hdl, &port->events, &port->wait_ovl) == 0) {
+			if (GetLastError() != ERROR_IO_PENDING)
+				RETURN_FAIL("WaitCommEvent() failed");
+		}
 	}
 
 	RETURN_INT(bytes_read);
